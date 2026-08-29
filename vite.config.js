d9716e1398a9220abc913158b8593c64ad9ea523
MuhammadShafeeq4547 <muhammadshafeeq4547@gmail.com>
2026-08-29 20:37:16 +0500

    fix vite build configuration

--- vite.config.js
+++ vite.config.js
@@ -2,28 +2,23 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss()
   ],
+
   server: {
-    host: true, // Allow access from mobile device
+    host: true,
     port: 5173
   },
+
   build: {
     minify: 'terser',
+
     terserOptions: {
       compress: {
-        drop_console: true, // Remove console logs in production
-      }
-    },
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'framer-motion': ['framer-motion']
-        }
+        drop_console: true
       }
     }
   }
